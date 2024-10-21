05821b25822834185053d2ee041afc84128ed4f5
ice-myles <96409608+ice-myles@users.noreply.github.com>
2024-10-21 22:06:56 +0300

    Work on comments.

--- server/ws/fixture/client.go
+++ server/ws/fixture/client.go
@@ -477,7 +477,7 @@ func LocalhostTLS() *tls.Config {
 	}
 
 	return &tls.Config{
-		MinVersion:   tls.VersionTLS13,
+		MinVersion:   tls.VersionTLS12,
 		RootCAs:      caCertPool,
 		Certificates: []tls.Certificate{cert},
 	}
