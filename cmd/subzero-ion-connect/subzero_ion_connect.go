78faf79bec2a2cb71e882e1be74f35c620c79b14
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2024-12-16 17:31:35 +0100

    comments

--- cmd/subzero-ion-connect/subzero_ion_connect.go
+++ cmd/subzero-ion-connect/subzero_ion_connect.go
@@ -43,10 +43,10 @@ var (
 func init() {
 	initFlags()
 	wsserver.RegisterReqMustAuthenticate(func(context.Context, *model.Subscription) (authRequired bool) {
-		return true
+		return false
 	})
 	wsserver.RegisterEventMustAuthenticate(func(context.Context, ...*model.Event) (authRequired bool) {
-		return true
+		return false
 	})
 	wsserver.RegisterWSEventListener(func(ctx context.Context, events ...*model.Event) error {
 		if err := command.AcceptEvents(ctx, events...); err != nil {
