ee0a4e50858b064d212811cf8ea21b69136e79f6
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-01-17 10:08:36 +0000

    dvm: check for auth errors (#78)

---
+++ cmd/subzero-ion-connect-authgen/subzero_ion_connect_keygen.go
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: ice License 1.0
+
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/nbd-wtf/go-nostr"
+
+	"github.com/ice-blockchain/subzero/model"
+)
+
+func main() {
+	relayURL := flag.String("relay", "", "URL of the relay")
+	challenge := flag.String("challenge", "", "Challenge string")
+	key := flag.String("key", model.GeneratePrivateKey(), "Private key")
+	flag.Parse()
+
+	if *relayURL == "" || *challenge == "" {
+		fmt.Println("Usage: subzero_ion_connect_keygen -relay <relay URL> -challenge <challenge string>")
+		return
+	}
+
+	public, err := model.GetPublicKey(*key)
+	if err != nil {
+		fmt.Println("Error getting public key:", err)
+		return
+	}
+
+	fmt.Println("Private key:", *key)
+	fmt.Println("Public key: ", public)
+
+	authEvent := model.Event{
+		Event: nostr.Event{
+			CreatedAt: nostr.Now(),
+			Kind:      nostr.KindClientAuthentication,
+			Tags: model.Tags{
+				{"relay", *relayURL},
+				{"challenge", *challenge},
+			},
+		},
+	}
+	err = authEvent.SignWithAlg(*key, model.SignAlgEDDSA, model.KeyAlgCurve25519)
+	if err != nil {
+		fmt.Println("Error signing event:", err)
+		return
+	}
+
+	msg := nostr.AuthEnvelope{Event: authEvent.Event}
+	fmt.Println(msg.String())
+}
