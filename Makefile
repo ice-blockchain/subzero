50622afd34540105e5d95bf2f5a13190c9f45e84
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-02-16 17:58:14 +0100

    database: rank: DDL

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ CGO_ENABLED ?= 1
 GOOS         ?=
 GOARCH       ?=
 SERVICE_NAME ?=
+TAGS         := go_json,linux,sqlite_stat4,sqlite_icu,sqlite_fts5,sqlite_math_functions
 
 export CGO_ENABLED GOOS GOARCH SERVICE_NAME
 
@@ -60,7 +61,7 @@ checkGenerated: generate
 	true;
 
 build-all@ci/cd:
-	go build -tags "go_json linux sqlite_stat4 sqlite_icu sqlite_fts5" -a -v -race ./...
+	go build -tags $(TAGS) -a -v -race ./...
 
 build: build-all@ci/cd
 
@@ -90,11 +91,11 @@ coverage: $(COVERAGE_FILE)
 
 test@ci/cd:
 	# TODO make -race work
-	go test -timeout 20m -tags "go_json test linux sqlite_stat4 sqlite_icu sqlite_fts5" -v -cover -coverprofile=$(COVERAGE_FILE) -covermode atomic
+	go test -timeout 20m -tags test,$(TAGS) -v -cover -coverprofile=$(COVERAGE_FILE) -covermode atomic
 
 benchmark@ci/cd:
 	# TODO make -race work
-	go test -timeout 20m -tags "go_json test linux sqlite_stat4 sqlite_icu sqlite_fts5" -run=^$ -v -bench=. -benchmem -benchtime 10s
+	go test -timeout 20m -tags test,$(TAGS) -run=^$ -v -bench=. -benchmem -benchtime 10s
 
 benchmark:
 	set -xe; \
@@ -164,7 +165,7 @@ buildAllBinaries:
 binary-specific-service:
 	set -xe; \
 	echo "$@: $(SERVICE_NAME) / $(GOOS) / $(GOARCH)" ; \
-	go build -tags "go_json linux sqlite_stat4 sqlite_icu sqlite_fts5" -a -v -o ./cmd/$${SERVICE_NAME}/bin ./cmd/$${SERVICE_NAME}; \
+	go build -tags $(TAGS) -a -v -o ./cmd/$${SERVICE_NAME}/bin ./cmd/$${SERVICE_NAME}; \
 	cp ./cmd/$${SERVICE_NAME}/bin ./$${SERVICE_NAME}.$${GOOS}.$${GOARCH}.bin; \
 
 all: checkLicense checkModVersion checkIfAllDependenciesAreUpToDate checkGenerated build test coverage benchmark clean
--- database/query/query.go
+++ database/query/query.go
@@ -23,6 +23,10 @@ import (
 
 const (
 	selectDefaultBatchLimit = 100
+
+	systemKindQuote        = 1
+	systemKindCommentRoot  = 2
+	systemKindCommentReply = 3
 )
 
 var (
@@ -38,6 +42,7 @@ type (
 	databaseEvent struct {
 		model.Event
 		SystemCreatedAt int64
+		SystemKind      sql.NullInt64
 		ReferenceID     sql.NullString
 		Jtags           string
 		SigAlg          string
@@ -63,6 +68,25 @@ type databaseBatchRequest struct {
 	Delete []databaseFilterDelete
 }
 
+func detectSystemKind(e *model.Event) (int64, bool) {
+	var hasReply, hasRoot bool
+	for i := range e.Tags {
+		switch e.Tags[i].Key() {
+		case "a", "e":
+			hasReply = hasReply || len(e.Tags[i]) > replyMarkerIndex && strings.EqualFold(e.Tags[i][replyMarkerIndex], "reply")
+			hasRoot = hasRoot || len(e.Tags[i]) > replyMarkerIndex && strings.EqualFold(e.Tags[i][replyMarkerIndex], "root")
+		case "q", "Q":
+			return systemKindQuote, true
+		}
+	}
+	if hasReply {
+		return systemKindCommentReply, true
+	} else if hasRoot {
+		return systemKindCommentRoot, true
+	}
+	return -1, false
+}
+
 func toDatabaseEvent(e *model.Event) (*databaseEvent, error) {
 	var deleted bool
 
@@ -85,10 +109,14 @@ func toDatabaseEvent(e *model.Event) (*databaseEvent, error) {
 		}
 	}
 
+	var systemKind sql.NullInt64
+	systemKind.Int64, systemKind.Valid = detectSystemKind(e)
+
 	return &databaseEvent{
 		Event:           *e,
 		MasterPubKey:    e.GetMasterPublicKey(),
 		SystemCreatedAt: time.Now().UnixNano(),
+		SystemKind:      systemKind,
 		Jtags:           string(jtags),
 		SigAlg:          sigAlg,
 		KeyAlg:          keyAlg,
@@ -324,14 +352,15 @@ func (db *dbClient) deleteEvents(ctx context.Context, filters []databaseFilterDe
 
 func (db *dbClient) saveEvents(ctx context.Context, events []databaseEvent) error {
 	const stmt = `insert into events
-	(kind, created_at, system_created_at, id, pubkey, master_pubkey, sig, sig_alg, key_alg, content, tags, d_tag, h_tag, reference_id, content_metadata, deleted)
+	(kind, created_at, system_created_at, system_kind, id, pubkey, master_pubkey, sig, sig_alg, key_alg, content, tags, d_tag, h_tag, reference_id, content_metadata, deleted)
 values
-	(:kind, :created_at, :system_created_at, :id, :pubkey, :master_pubkey, :sig, :sig_alg, :key_alg, :content, :jtags, :d_tag, :h_tag, :reference_id, :content_metadata, :deleted)
+	(:kind, :created_at, :system_created_at, :system_kind, :id, :pubkey, :master_pubkey, :sig, :sig_alg, :key_alg, :content, :jtags, :d_tag, :h_tag, :reference_id, :content_metadata, :deleted)
 on conflict do update set
 	id                = excluded.id,
 	kind              = excluded.kind,
 	created_at        = excluded.created_at,
 	system_created_at = excluded.system_created_at,
+	system_kind       = excluded.system_kind,
 	pubkey            = excluded.pubkey,
 	master_pubkey     = excluded.master_pubkey,
 	sig               = excluded.sig,
