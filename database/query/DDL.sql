550bb657ad056dc82be262906420bf3a0b179f43
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-02-17 16:49:10 +0100

    comments

--- database/query/DDL.sql
+++ database/query/DDL.sql
@@ -565,7 +565,7 @@ CREATE TABLE IF NOT EXISTS ranked_events
     score             real    not null
 ) strict, WITHOUT ROWID;
 --------
-create index if not exists ranked_events_points_ix           on ranked_events(points);
+create index if not exists ranked_events_points_ix           on ranked_events(points) where points <= 0;
 create index if not exists ranked_events_score_ix            on ranked_events(score desc);
 create index if not exists ranked_events_created_at_score_ix on ranked_events(event_created_at desc, score desc);
 --------
