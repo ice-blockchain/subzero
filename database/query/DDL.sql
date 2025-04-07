70a67be5240ef379c68753d8a5d37e2fd8cda4a0
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-04-07 14:14:05 +0300

    fix ddl

--- database/query/DDL.sql
+++ database/query/DDL.sql
@@ -364,6 +364,12 @@ CREATE TABLE IF NOT EXISTS replaceable_events_before_update
 AS TABLE events;
 ALTER TABLE replaceable_events_before_update
     ADD COLUMN IF NOT EXISTS replaced_by_id TEXT;
+DO $$ BEGIN
+    if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'replaceable_events_before_update' and constraint_type = 'PRIMARY KEY') then
+        ALTER TABLE replaceable_events_before_update
+            ADD CONSTRAINT replaceable_events_before_update_pkey PRIMARY KEY(id);
+    end if;
+END $$;
 CREATE INDEX IF NOT EXISTS idx_replaceable_events_before_update_ ON replaceable_events_before_update(replaced_by_id);
 
 CREATE OR REPLACE FUNCTION events_store_replaceable_data_before_update()
