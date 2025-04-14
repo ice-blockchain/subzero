ce9cd611c2e8ee953757116ae9137d782ee917ab
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-04-14 12:45:26 +0300

    fix rebase, comments, ddl

--- database/query/DDL.sql
+++ database/query/DDL.sql
@@ -371,13 +371,14 @@ EXECUTE FUNCTION trigger_events_before_delete_remove_tags_explicit();
 --------
 CREATE TABLE IF NOT EXISTS replaceable_events_before_update
 AS TABLE events;
-ALTER TABLE replaceable_events_before_update
-    ADD COLUMN IF NOT EXISTS replaced_by_id TEXT;
 DO $$ BEGIN
+    ALTER TABLE replaceable_events_before_update
+        ADD COLUMN IF NOT EXISTS replaced_by_id TEXT NOT NULL DEFAULT '';
     if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'replaceable_events_before_update' and constraint_type = 'PRIMARY KEY') then
         ALTER TABLE replaceable_events_before_update
             ADD CONSTRAINT replaceable_events_before_update_pkey PRIMARY KEY(id, replaced_by_id);
     end if;
+    ALTER TABLE replaceable_events_before_update ALTER COLUMN replaced_by_id DROP DEFAULT;
 END $$;
 CREATE INDEX IF NOT EXISTS idx_replaceable_events_before_update_ ON replaceable_events_before_update(replaced_by_id);
 
