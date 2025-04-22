1963f9146870fd4f32153dd980fcfac3de7927d3
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-04-22 17:32:16 +0300

    auto-generated files fix.

--- database/query/query_invalid_tokens_test.go
+++ database/query/query_invalid_tokens_test.go
@@ -7,9 +7,10 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/ice-blockchain/subzero/model"
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ice-blockchain/subzero/model"
 )
 
 func TestMarkTokenAsInvalid(t *testing.T) {
