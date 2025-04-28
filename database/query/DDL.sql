3aa94ad432b6f1b38f05e9db3fdcc261751d8786
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-04-28 15:30:55 +0300

    Added missed index.

--- database/query/DDL.sql
+++ database/query/DDL.sql
@@ -132,6 +132,7 @@ DO $$ BEGIN
 END $$;
 --------
 --- TODO: optimize index size and usage.
+create index if not exists idx_event_tags_id                          on event_tags(id);
 create index if not exists idx_event_tags_key_value1                  on event_tags(event_tag_key, event_tag_value1);
 create index if not exists idx_event_tags_key_value2                  on event_tags(event_tag_key, event_tag_value2);
 create index if not exists idx_event_tags_key_value3                  on event_tags(event_tag_key, event_tag_value3);
