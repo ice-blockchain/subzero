2d2ce0b78b402b809d73387a98cef08aa66c054c
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-05-28 18:28:08 +0200

    Fix database and validator (#252)

--- database/query/DDL.sql
+++ database/query/DDL.sql
@@ -813,9 +813,15 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER trigger_events_before_insert_check_onbehalf_permission
-BEFORE INSERT OR UPDATE ON events
+BEFORE INSERT ON events
 FOR EACH ROW
 WHEN (NEW.master_pubkey != NEW.pubkey AND (NEW.pubkey != '' AND NEW.master_pubkey != ''))
+EXECUTE FUNCTION trigger_events_before_insert_check_onbehalf_permission();
+
+CREATE OR REPLACE TRIGGER trigger_events_before_update_check_onbehalf_permission
+BEFORE UPDATE ON events
+FOR EACH ROW
+WHEN ((NEW.master_pubkey != NEW.pubkey AND (NEW.pubkey != '' AND NEW.master_pubkey != '')) AND (OLD.id != NEW.id))
 EXECUTE FUNCTION trigger_events_before_insert_check_onbehalf_permission();
 --------
 CREATE OR REPLACE FUNCTION subzero_nostr_attestation_update_is_allowed(
