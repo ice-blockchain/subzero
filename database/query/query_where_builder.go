ice-blockchain/subzero

commit bbe6801bff4744d02831630df807a05a788e71ac

database: add filter for `negative kinds` (#283)

database/query/query_where_builder.go
@@ -218,8 +218,12 @@ func (b *sliceBuilder[T]) Build(builder *queryBuilder, filterID string, name str
 		builder.WriteRune(':')
 		builder.WriteString(builder.PushValue(filterID, b.ParamName, s[0]))
 	} else {
-		// X = ANY(...).
-		builder.WriteString("ANY(:")
+		// X = ANY/ALL(...).
+		if b.Negative {
+			builder.WriteString("ALL(:")
+		} else {
+			builder.WriteString("ANY(:")
+		}
 		builder.WriteString(builder.PushValue(filterID, b.ParamName, s))
 		builder.WriteRune(')')
 	}
@@ -632,6 +636,29 @@ func (b *queryBuilder) ApplySpecialKinds(filter *databaseFilterSearch) (kinds []
 	return kinds
 }
 
+func (b *queryBuilder) ApplyKinds(filter *databaseFilterSearch, kinds []int) {
+	if len(kinds) == 0 {
+		return
+	}
+
+	var negative, positive []int
+	for _, kind := range kinds {
+		if kind < 0 {
+			negative = append(negative, -kind)
+		}
+		if kind > 0 {
+			positive = append(positive, kind)
+		}
+	}
+
+	buildFromSlice(b, sqlOpCodeAND, filter.ID, positive, "e.kind", "kind_positive")
+	if len(positive) == 0 {
+		// Include `negative` kinds only if there are no positive ones, because it does not make sense to
+		// have both positive and negative kinds in the same filter.
+		buildFromSliceNegative(b, sqlOpCodeAND, filter.ID, negative, "e.kind", "kind_negative")
+	}
+}
+
 func (b *queryBuilder) ApplyFilter(filter *databaseFilterSearch) error {
 	if isFilterEmpty(filter) {
 		return nil
@@ -640,7 +667,7 @@ func (b *queryBuilder) ApplyFilter(filter *databaseFilterSearch) error {
 	b.WriteRune('(') // Begin the filter section.
 	buildFromSlice(b, sqlOpCodeNONE, filter.ID, filter.IDs, "e.id", "")
 	buildFromSlice(b, sqlOpCodeAND, filter.ID, filter.Addresses, "e.address", "")
-	buildFromSlice(b, sqlOpCodeAND, filter.ID, b.ApplySpecialKinds(filter), "e.kind", "")
+	b.ApplyKinds(filter, b.ApplySpecialKinds(filter))
 	b.ApplyFilterForExtensions(filter)
 	b.ApplyFilterTtags(filter)
 	if len(filter.Authors) > 0 {

database/query/query_test.go
@@ -1508,6 +1508,31 @@ func TestSelectSoftDeletedPosts(t *testing.T) {
 	})
 }
 
+func TestSelectPositiveNegativeKinds(t *testing.T) {
+	t.Parallel()
+
+	db := helperNewDatabase(t)
+	defer db.Close()
+
+	t.Run("Add events", func(t *testing.T) {
+		var ev1, ev2, ev3 model.Event
+		ev1.Kind, ev2.Kind, ev3.Kind = nostr.KindTextNote, nostr.KindArticle, model.CustomIONKindEditableTextNote
+		ev1.CreatedAt, ev2.CreatedAt, ev3.CreatedAt = nostr.Now(), nostr.Now(), nostr.Now()
+		ev1.Content, ev2.Content, ev3.Content = "text note", "article", "editable text note"
+		helperSignAndSaveEvent(t, db, "", &ev1, &ev2, &ev3)
+	})
+	t.Run("Select NOT text notes", func(t *testing.T) {
+		events := helperSelectEvents(t, db, model.Filter{Kinds: []int{-nostr.KindTextNote, -model.CustomIONKindEditableTextNote}})
+		require.Len(t, events, 1, "should return only article")
+		require.Equal(t, nostr.KindArticle, events[0].Kind)
+	})
+	t.Run("Select all kinds except articles", func(t *testing.T) {
+		events := helperSelectEvents(t, db, model.Filter{Kinds: []int{-nostr.KindArticle}})
+		require.Len(t, events, 2, "should return text note and editable text note")
+		require.ElementsMatch(t, []int{nostr.KindTextNote, model.CustomIONKindEditableTextNote}, []int{events[0].Kind, events[1].Kind})
+	})
+}
+
 func TestSelectRankTopEvents(t *testing.T) {
 	t.Parallel()
 

go.mod
@@ -55,10 +55,10 @@ require (
 	github.com/testcontainers/testcontainers-go/modules/postgres v0.37.0
 	github.com/u2takey/ffmpeg-go v0.5.0
 	github.com/xssnick/tonutils-go v1.13.0
-	github.com/xssnick/tonutils-storage v1.0.5
+	github.com/xssnick/tonutils-storage v1.1.0
 	go.uber.org/goleak v1.3.0
 	golang.org/x/net v0.41.0
-	google.golang.org/api v0.236.0
+	google.golang.org/api v0.237.0
 )
 
 require (

go.sum
@@ -871,8 +871,8 @@ golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8T
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df/go.mod h1:K8+ghG5WaK9qNqU5K3HdILfMLy1f3aNYFI/wnl100a8=
-google.golang.org/api v0.236.0 h1:CAiEiDVtO4D/Qja2IA9VzlFrgPnK3XVMmRoJZlSWbc0=
-google.golang.org/api v0.236.0/go.mod h1:X1WF9CU2oTc+Jml1tiIxGmWFK/UZezdqEu09gcxZAj4=
+google.golang.org/api v0.237.0 h1:MP7XVsGZesOsx3Q8WVa4sUdbrsTvDSOERd3Vh4xj/wc=
+google.golang.org/api v0.237.0/go.mod h1:cOVEm2TpdAGHL2z+UwyS+kmlGr3bVWQQ6sYEqkKje50=
 google.golang.org/appengine/v2 v2.0.6 h1:LvPZLGuchSBslPBp+LAhihBeGSiRh1myRoYK4NtuBIw=
 google.golang.org/appengine/v2 v2.0.6/go.mod h1:WoEXGoXNfa0mLvaH5sV3ZSGXwVmy8yf7Z1JKf3J3wLI=
 google.golang.org/genproto v0.0.0-20250603155806-513f23925822 h1:rHWScKit0gvAPuOnu87KpaYtjK5zBMLcULh7gxkCXu4=

commit bf2f524faa29efeb139a52decd296854f850e640

database: fix kind 0 lookup (#286)
Was broken in bbe6801

database/query/query_where_builder.go
@@ -642,8 +642,7 @@ func (b *queryBuilder) ApplyKinds(filter *databaseFilterSearch, kinds []int) {
 	for _, kind := range kinds {
 		if kind < 0 {
 			negative = append(negative, -kind)
-		}
-		if kind > 0 {
+		} else {
 			positive = append(positive, kind)
 		}
 	}