752ee2b048549c853b6c6f3e50ce70fb4bb89880
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-07-18 16:20:13 +0200

    server: generate self-self signed certificate or request

--- server/server_test.go
+++ server/server_test.go
@@ -6,7 +6,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 )
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
 
 func TestExtractServerNameFromRelayURL(t *testing.T) {
 	t.Parallel()
