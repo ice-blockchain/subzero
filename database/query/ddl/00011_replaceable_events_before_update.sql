d927f2dbed3a1cc6148afb69d7fe446afd2c280b
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-07-23 19:20:26 +0200

    comments

--- database/query/ddl/00011_replaceable_events_before_update.sql
+++ database/query/ddl/00011_replaceable_events_before_update.sql
@@ -1,16 +1,15 @@
 -- SPDX-License-Identifier: ice License 1.0
-
-CREATE TABLE IF NOT EXISTS replaceable_events_before_update AS TABLE events;
-
 DO $$
 BEGIN
-    ALTER TABLE replaceable_events_before_update
-        ADD COLUMN IF NOT EXISTS replaced_by_id TEXT NOT NULL DEFAULT '';
-    if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'replaceable_events_before_update' and constraint_type = 'PRIMARY KEY') then
+    CREATE TABLE IF NOT EXISTS replaceable_events_before_update AS TABLE events;
+    IF NOT EXISTS (select constraint_name from information_schema.table_constraints where table_name = 'replaceable_events_before_update' and constraint_type = 'PRIMARY KEY') then
+        ALTER TABLE replaceable_events_before_update
+            ADD COLUMN IF NOT EXISTS replaced_by_id TEXT NOT NULL DEFAULT '';
         ALTER TABLE replaceable_events_before_update
             ADD CONSTRAINT replaceable_events_before_update_pkey PRIMARY KEY(id, replaced_by_id);
-    end if;
-    ALTER TABLE replaceable_events_before_update ALTER COLUMN replaced_by_id DROP DEFAULT;
+        ALTER TABLE replaceable_events_before_update
+            ALTER COLUMN replaced_by_id DROP DEFAULT;
+    END IF;
 END $$;
 --------
 CREATE INDEX IF NOT EXISTS idx_replaceable_events_before_update_ ON replaceable_events_before_update(replaced_by_id);
