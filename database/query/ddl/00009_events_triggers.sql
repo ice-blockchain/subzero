dfcc9d6bc311d0ac7526d48de604d2d936918991
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-07-31 19:17:45 +0300

    Fixed update event_tags token tag on update trigger.

--- database/query/ddl/00009_events_triggers.sql
+++ database/query/ddl/00009_events_triggers.sql
@@ -70,7 +70,7 @@ BEGIN
         COALESCE(value->>5, '')
     FROM jsonb_array_elements(COALESCE(NEW.tags, '[]'::jsonb)) AS value
     WHERE
-        length(value->>0) = 1 OR value->>0 in ('summary', 'name', 'description', 'title', 'poll')
+        length(value->>0) = 1 OR value->>0 in ('summary', 'name', 'description', 'title', 'poll', 'token')
     ON CONFLICT(event_id, event_tag_key, event_tag_value1, event_tag_value3) DO NOTHING;
 
     RETURN NEW;
--- go.mod
+++ go.mod
@@ -91,7 +91,7 @@ require (
 	github.com/MicahParks/keyfunc v1.9.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/andybalholm/brotli v1.2.0 // indirect
-	github.com/aws/aws-sdk-go v1.55.7 // indirect
+	github.com/aws/aws-sdk-go v1.55.8 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.5 // indirect
