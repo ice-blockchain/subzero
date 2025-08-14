faebbfdfd8e0156ea9255f33b527fc9424a38be9
Cronus <105345303+ice-cronus@users.noreply.github.com>
2025-08-15 00:36:03 +0700

    rebase changes to latest storage lib, fixes for latest (#393)

--- storage/download.go
+++ storage/download.go
@@ -17,7 +17,7 @@ import (
 	"github.com/cockroachdb/errors"
 	"github.com/imroc/req/v3"
 	"github.com/nbd-wtf/go-nostr"
-	"github.com/xssnick/tonutils-go/adnl"
+	"github.com/xssnick/tonutils-go/adnl/keys"
 	"github.com/xssnick/tonutils-storage/storage"
 	"golang.org/x/sync/errgroup"
 
@@ -227,7 +227,7 @@ func (c *client) connectToBootstrap(ctx context.Context, torrent *storage.Torren
 		if err != nil {
 			return errors.Wrapf(err, "failed to decode bootstrap %v, invalid pubkey %v", string(b64), string(pk))
 		}
-		bs.Overlay.ID = adnl.PublicKeyED25519{Key: pubKey}
+		bs.Overlay.ID = keys.PublicKeyED25519{Key: pubKey}
 		if err = c.server.ConnectToNode(ctx, torrent, bs.Overlay, bs.DHT.AddrList); err != nil {
 			return errors.Wrapf(err, "failed to connect to bootstrap node %#v", bs.DHT.AddrList.Addresses[0])
 		}
--- storage/upload.go
+++ storage/upload.go
@@ -23,8 +23,8 @@ import (
 
 	"github.com/cockroachdb/errors"
 	gomime "github.com/cubewise-code/go-mime"
-	"github.com/xssnick/tonutils-go/adnl"
 	"github.com/xssnick/tonutils-go/adnl/dht"
+	"github.com/xssnick/tonutils-go/adnl/keys"
 	"github.com/xssnick/tonutils-go/adnl/overlay"
 	"github.com/xssnick/tonutils-go/tl"
 	"github.com/xssnick/tonutils-storage/storage"
@@ -210,7 +210,7 @@ func (c *client) buildBootstrapNodeInfo(tr *storage.Torrent) (*Bootstrap, error)
 	addr := c.gateway.GetAddressList()
 
 	dNode := dht.Node{
-		ID:        adnl.PublicKeyED25519{Key: key.Public().(ed25519.PublicKey)},
+		ID:        keys.PublicKeyED25519{Key: key.Public().(ed25519.PublicKey)},
 		AddrList:  &addr,
 		Version:   int32(time.Now().Unix()),
 		Signature: nil,
