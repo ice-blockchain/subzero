7a258a2e6c83752a37264e5f8f1fe6b66f4ba9c9
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-08-25 13:59:22 +0300

    Remove drop pgronga extension.

--- database/query/ddl/00003_events_indexes.sql
+++ database/query/ddl/00003_events_indexes.sql
@@ -35,7 +35,6 @@ CREATE INDEX IF NOT EXISTS idx_events_expiration_id ON events(expiration, id) WH
 
 -- TODO: remove pgroonga DROPs after the migration on all envs.
 DROP INDEX IF EXISTS idx_events_lookup_pgroonga;
-DROP EXTENSION IF EXISTS pgroonga;
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE INDEX IF NOT EXISTS idx_events_lookup_trgm ON events USING gin (lookup gin_trgm_ops);
