311cb390c75ae008e8d265ab52829c1b5b1b6de0
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-10-01 10:01:42 +0200

    database: improve delete trigger

--- database/query/ddl/00004_event_tags.sql
+++ database/query/ddl/00004_event_tags.sql
@@ -12,4 +12,15 @@ CREATE TABLE IF NOT EXISTS event_tags
     event_tag_value5  text not null DEFAULT '',
     primary key (event_id, event_tag_key, event_tag_value1, event_tag_value3)
 ) WITH (FILLFACTOR = 90);
+--------
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'event_tags_event_id_fkey_v2') THEN
+        ALTER TABLE event_tags
+            DROP CONSTRAINT IF EXISTS event_tags_event_id_fkey;
+        ALTER TABLE event_tags
+            ADD CONSTRAINT event_tags_event_id_fkey_v2 FOREIGN KEY (event_id)
+            REFERENCES events (id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
+    END IF;
+END $$;
 
--- database/query/ddl/00009_events_triggers.sql
+++ database/query/ddl/00009_events_triggers.sql
@@ -38,7 +38,46 @@ EXECUTE FUNCTION trigger_events_after_insert_generate_tags();
 CREATE OR REPLACE FUNCTION trigger_events_before_update_remove_old_data()
 RETURNS TRIGGER AS $$
 BEGIN
-    DELETE FROM event_tags WHERE event_id IN (NEW.id, OLD.id);
+    IF NEW.deleted = TRUE AND OLD.deleted = FALSE THEN
+        DELETE
+        FROM
+            event_tags
+        WHERE
+            event_id = OLD.id;
+        RETURN NEW;
+    END IF;
+
+    WITH new_parsed_tags AS (
+        SELECT
+            value->>0 as event_tag_key,
+            COALESCE(value->>1, '') as event_tag_value1,
+            COALESCE(value->>2, '') as event_tag_value2,
+            COALESCE(value->>3, '') as event_tag_value3,
+            COALESCE(value->>4, '') as event_tag_value4,
+            COALESCE(value->>5, '') as event_tag_value5
+        FROM
+            jsonb_array_elements(COALESCE(NEW.tags, '[]'::jsonb)) AS value
+        WHERE
+            length(value->>0) = 1
+            OR value->>0 IN ('summary', 'name', 'description', 'title', 'poll', 'ox', 'token', 'relay')
+    )
+    -- Delete old tags that are not present in the new set of tags.
+    DELETE
+    FROM
+        event_tags et
+    WHERE
+        et.event_id = OLD.id
+        AND (et.event_tag_key, et.event_tag_value1, et.event_tag_value3) IN (
+            SELECT event_tag_key, event_tag_value1, event_tag_value3
+            FROM event_tags
+            WHERE event_id = OLD.id
+
+            EXCEPT
+
+            SELECT event_tag_key, event_tag_value1, event_tag_value3
+            FROM new_parsed_tags
+    );
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
