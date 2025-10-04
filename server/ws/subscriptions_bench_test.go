84a8210723bbb9fcc3ace7e360f6f39bda888979
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-10-04 09:18:32 +0200

    ws: switch sync map implementaion
    ```
    benchmark                            old ns/op     new ns/op     delta
    BenchmarkAuthLoadAndBroadcast-16     7861241       1845230       -76.53%
    BenchmarkAuthLoadAndBroadcast-16     8021519       1828466       -77.21%
    BenchmarkAuthLoadAndBroadcast-16     7923084       1834263       -76.85%
    BenchmarkAuthLoadAndBroadcast-16     7926571       1875563       -76.34%
    BenchmarkAuthLoadAndBroadcast-16     7923509       1920142       -75.77%
    BenchmarkAuthLoadAndBroadcast-16     8150643       1824405       -77.62%
    ```

--- server/ws/subscriptions_bench_test.go
+++ server/ws/subscriptions_bench_test.go
@@ -3,9 +3,14 @@
 package ws
 
 import (
+	"context"
+	"math/rand"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/nbd-wtf/go-nostr"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ice-blockchain/subzero/model"
 )
@@ -37,6 +42,63 @@ func BenchmarkCanForwardEvent(b *testing.B) {
 		b.ReportAllocs()
 		for b.Loop() {
 			canForwardEvent(&ev, nil, "master", "deviceKey")
+		}
+	})
+}
+
+type dummyWriter struct{}
+
+func (*dummyWriter) WriteMessage(context.Context, int, []byte) error { return nil }
+func (*dummyWriter) Close() error                                    { return nil }
+
+func BenchmarkAuthLoadAndBroadcast(b *testing.B) {
+	const subscriptionCount = 500_000
+
+	h := newHandler("", "")
+	require.NotNil(b, h)
+
+	writers := make([]Writer, subscriptionCount)
+	spareWriters := make([]Writer, subscriptionCount)
+	for idx := range subscriptionCount {
+		w := new(dummyWriter)
+		h.linkSubscription(w, &model.Subscription{
+			ID: strconv.Itoa(idx),
+			Filters: model.Filters{
+				{
+					Kinds: []int{nostr.KindTextNote},
+					Since: model.PointerOf(nostr.Now().Add(time.Hour)),
+				},
+			},
+		})
+		h.ConnAuth.Store(w, connAuthData{Challenge: "challenge-" + strconv.Itoa(idx)})
+		writers[idx] = w
+		spareWriters[idx] = new(dummyWriter)
+	}
+
+	var ev model.Event
+	ev.Kind = nostr.KindArticle
+	ev.CreatedAt = nostr.Now()
+	ev.Tags = model.Tags{
+		{"k", "1"},
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		var opCount int
+		localRand := rand.New(rand.NewSource(rand.Int63()))
+
+		for pb.Next() {
+			writer := writers[localRand.Intn(len(writers))]
+			_, _ = h.ConnAuth.Load(writer)
+
+			if opCount%3 == 0 {
+				nextWriter := spareWriters[localRand.Intn(len(spareWriters))]
+				h.ConnAuth.Store(nextWriter, connAuthData{})
+			} else {
+				_ = h.BroadcastNewEvents(b.Context(), &ev)
+			}
+			opCount++
 		}
 	})
 }
