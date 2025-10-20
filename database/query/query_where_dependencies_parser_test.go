91b87a3722a4627b74fbae7fafb6df9fc26a0f74
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2025-10-20 15:13:58 +0200

    database: revert kind3 changes (#494)

--- database/query/query_where_dependencies_parser_test.go
+++ database/query/query_where_dependencies_parser_test.go
@@ -1899,15 +1899,10 @@ func TestReduceKind3EventsFromFollowersQuery(t *testing.T) {
 			Tags:   model.TagMap{}.SetLiterals("p", userPub),
 		}
 		events := helperSelectEvents(t, db, f)
-		if false {
-			require.Len(t, events, 4) // 2 follow lists embedding, 2 metadata.
-			require.EqualValues(t, []*model.Event{&meta2, &meta1}, events[2:])
-			require.Equal(t, model.CustomIONKindEphemeralEmbedding, events[0].Kind)
-			require.Equal(t, model.CustomIONKindEphemeralEmbedding, events[1].Kind)
-		} else {
-			t.Logf("kind3 must be excluded")
-			require.EqualValues(t, []*model.Event{&meta2, &meta1}, events)
-		}
+		require.Len(t, events, 4) // 2 follow lists embedding, 2 metadata.
+		require.EqualValues(t, []*model.Event{&meta2, &meta1}, events[2:])
+		require.Equal(t, model.CustomIONKindEphemeralEmbedding, events[0].Kind)
+		require.Equal(t, model.CustomIONKindEphemeralEmbedding, events[1].Kind)
 
 		f.Kinds = nil
 		events = helperSelectEvents(t, db, f)
