d6139ff7129fd739039edc353558d495069e6f5d
ice-midas <170405428+ice-midas@users.noreply.github.com>
2025-12-17 11:31:25 +0100

    fix remaining CommitEvents (#521)

--- cmd/subzero-ion-connect/subzero_ion_connect.go
+++ cmd/subzero-ion-connect/subzero_ion_connect.go
@@ -131,10 +131,6 @@ func init() {
 		return nil
 	})
 	command.RegisterCommitListener(func(ctx context.Context, events ...*model.Event) error {
-		if err := query.CommitEvents(ctx, events...); err != nil {
-			return errors.Wrapf(err, "failed to delete outdated replaced events")
-		}
-
 		if err := storage.AcceptEvents(ctx, events...); err != nil {
 			return errors.Wrapf(err, "storage.AcceptEvents failed: %s", model.Events(events).String())
 		}
