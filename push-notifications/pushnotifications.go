88874ac146a5a851c48ab70002a8a91c4ec19988
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2026-02-18 19:38:53 +0100

    comments

--- push-notifications/pushnotifications.go
+++ push-notifications/pushnotifications.go
@@ -368,12 +368,12 @@ func AcceptEventsFromBroadcast(ctx context.Context, events ...*model.Event) erro
 func AcceptEvents(ctx context.Context, events ...*model.Event) error {
 	var err error
 
-	hasOnlyEphemeralEvents := !slices.ContainsFunc(events, func(event *model.Event) bool {
+	hasNonEphemeralEvent := slices.ContainsFunc(events, func(event *model.Event) bool {
 		return event.Kind != model.CustomIONKindEphemeralEmbedding
 	})
 
 	// Ephemeral embedding batch may come only from broadcaster, so we handle it separately.
-	if hasOnlyEphemeralEvents && len(events) > 0 {
+	if !hasNonEphemeralEvent && len(events) > 0 {
 		return globalPushNotificationManager.AcceptEventsFromBroadcast(ctx, events)
 	}
 
