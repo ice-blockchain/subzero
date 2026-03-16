a3bbc86cbfbd08e8600a44ab3ff78bb4bab016ba
Dionysos <75300347+ice-dionysos@users.noreply.github.com>
2026-03-16 16:29:24 +0100

    Fix pn (#605)

--- cmd/subzero-ion-connect-indexer/subzero_ion_connect_indexer.go
+++ cmd/subzero-ion-connect-indexer/subzero_ion_connect_indexer.go
@@ -191,10 +191,11 @@ func init() {
 	})
 	wsserver.RegisterWSSubscriptionListener(query.GetStoredEvents, dvm.GetStoredEvents)
 
-	push := func(ctx context.Context, events ...*model.Event) error {
+	push := func(taskCtx context.Context, events ...*model.Event) error {
+		ctx := context.WithoutCancel(taskCtx)
 		antsPool.Submit(func() {
 			start := time.Now()
-			n := webserver.BroadcastNewEvents(context.WithoutCancel(ctx), events...)
+			n := webserver.BroadcastNewEvents(ctx, events...)
 			end := time.Since(start)
 			log.Trace().
 				Int("event_count", len(events)).
